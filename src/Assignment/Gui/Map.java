158659881e37af3dde7a306854c67949d6d2702c
Nandeesh Shirangala Bhujanga <nandeesh.shirangalabhujanga@my.jcu.edu.au>
2019-10-30 23:23:58 +1000

    Road Map added

---
+++ src/Assignment/Gui/Map.java
@@ -0,0 +1,55 @@
+package Assignment.Gui;
+
+import Assignment.Road;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.awt.Color;
+
+
+public class Map extends JPanel {
+    public static final Color DARK_GREEN = new Color(0, 51, 0);
+    ArrayList<RoadShape> roads = new ArrayList<>();
+    ArrayList<VehicleImage> vehicleImages = new ArrayList<>();
+    ArrayList<TrafficLightImage> trafficLightImages = new ArrayList<>();
+
+    public Map(int length, int width) {
+
+        setBackground(DARK_GREEN);
+        // setPreferredSize(new Dimension(length, width));
+    }
+
+    /*public void paintRoads() {
+       // repaint();
+        System.out.println("LALALALALA");
+    }*/
+
+    public void addRoad(int length, int width, String direction, int xinit, int yinit, int roadnum) {
+        this.roads.add(new RoadShape(length, width, direction, xinit, yinit, roadnum));
+        repaint();
+    }
+
+    void addVehicle(String type, int yNorth, int xWest, int length, String direction) {
+        int width = 10;
+        switch (direction) {
+            case "North":
+            case "South":
+                vehicleImages.add(new VehicleImage(xWest, yNorth, width, length, type));
+                break;
+            case "East":
+            case "West":
+                vehicleImages.add(new VehicleImage(xWest, yNorth, length, width, type));
+                break;
+        }
+
+
+        vehicleImages.add(new VehicleImage(xWest, -yNorth, length, 10, type));
+    }
+
+    void addTrafficLight(int xWest, int yNorth, boolean status) {
+        trafficLightImages.add(new TrafficLightImage(xWest, yNorth, status));
+    }
+
+
+}
