07d65f146bda5cf5b148b6bbe138f783ff5fce30
Nandeesh Shirangala Bhujanga <nandeesh.shirangalabhujanga@my.jcu.edu.au>
2019-11-01 21:03:37 +1000

    new direction added to road class

--- src/Assignment/Road.java
+++ src/Assignment/Road.java
@@ -130,13 +130,12 @@ public class Road {
                 break;
         }
         vehicles.get(vehicles.size() - 1).intiDir();
-        // vehicles.get(vehicleRef).intiDir();
-        // vehicleRef = vehicles.size();
+
         vehiclesOnRoad++;
     }
 
     void addBus(int speed) {
-        /*Adds a bus to the beginning of the road in the direction of the road at a specified speed*/
+
         switch (direction) {
             case "North":
                 vehicles.add(new Bus("North", speed, xwest + width / 2, ysouth));
@@ -157,7 +156,7 @@ public class Road {
 
 
     void addTrafficLight(String which_end) {
-        /*Adds a traffic light to the end or beginning of the road */
+
         if (which_end.equals("Start")) {
             this.trafficLightStart = true;
             this.startLight = new TrafficLight("Start");
@@ -170,28 +169,26 @@ public class Road {
     }
 
     void setEndLight(boolean status) {
-        /*Change the status of the traffic light at the end of the road*/
+
         if (trafficLightend) {
             endLight.setStatus(status);
         }
     }
 
     public void setStartLight(boolean status) {
-        /*Change the status of the traffic light at the beginning of the road*/
+
         if (trafficLightStart) {
             startLight.setStatus(status);
         }
     }
 
     void moveVehicles() {
-        /*Moves all the vehicles currently on the road. Ensures that there are no collisions
-         * and that the vehicles are not moving through red lights*/
+
 
         for (int i = 0; i < vehicles.size(); i++) {
             if (checkCollision(i)) {
                 System.out.println("\n\n\n\n...........................................................................Collision with " + vehicles.get(i).getType() + "  " + vehicles.get(i-1).getType());
-                //vehicles.get(i).setSpeed(vehicles.get(i - 1).getSpeed());
-                //vehicles.get(i).setPos(vehicles.get(i - 1).getRear());
+
             } else if (checkMovement(i)) {
                 if (trafficLightend) {
                     if (endLight.isGreen()) {
@@ -204,15 +201,9 @@ public class Road {
         }
     }
 
-    /*boolean freeSpace(){
-        boolean status = false;
-        if (vehicles.get(vehicles.size()-1).getDistanceTraveled()<)
-    }*/
 
     private boolean checkCollision(int vRef) {
-        /*Returns true if a vehicle will move past the vehicle
-         * in front of it. If it does, position is set to the end of front vehicle
-         * and speed is set to the first vehicles speed. Used in move vehicles function */
+
         boolean status = false;
         if (vRef != 0){
 
@@ -254,13 +245,6 @@ public class Road {
         return status;
     }
 
-    /*private boolean checkMovement(int i) {
-        boolean status = false;
-        if (vehicles.get(i).getDistanceTraveled() > vehicles.get(i).getLength() + 15) {
-            status = true;
-        }
-        return status;
-    }*/
 
 
     boolean lightAtStart() {
@@ -281,7 +265,7 @@ public class Road {
 
     private boolean checkMovement(int i) {
 
-         /*Returns true if the vehicle will be moving off the current road*/
+
 
         boolean status = false;
 
@@ -309,7 +293,7 @@ public class Road {
 
 
     int getRoadEnd() {
-        /*Returns the position of the end of the road based on its direction*/
+
         int max = 0;
         if (direction.equals("North")) {
             max = ynorth;
@@ -329,54 +313,54 @@ public class Road {
     }
 
     Vehicle getVehicleOnRoad(int ref_num) {
-        /*Returns a specified vehicle currently on the road*/
+
         return vehicles.get(ref_num);
 
     }
 
 
     private void isOffRoad(int i) {
-        /*Adds a vehicle to the liost of vehicles that would be moving off the road*/
+
         vehiclesOffRoad.add(vehicles.get(i));
     }
 
 
 
     public ArrayList<Vehicle> getVehicles_off_road() {
-        /*Returns list of vehicles moving off the current road. Used to determine if vehicles will be turning etc.*/
+
         return vehiclesOffRoad;
     }
 
 
     void reset_offRoad() {
-        /*Resets the vehicles off road list*/
+
         vehiclesOffRoad.clear();
     }
 
 
     public int getLength() {
-        /*Returns length of the road*/
+
         return length;
     }
 
     public String getDirection() {
-        /*Returns the direction of the road*/
+
         return direction;
     }
 
     int getNum_vehicles() {
-        /*Returns the number of vehicles currently on the road*/
+
         return vehiclesOnRoad;
     }
 
 
     public int getXeast() {
-        /*Returns the coordinate of east edge of road*/
+
         return xeast;
     }
 
     public int getXwest() {
-        /*Returns the coordinate of west edge of road*/
+
         return xwest;
     }
 
@@ -385,12 +369,12 @@ public class Road {
     }
 
     public int getYnorth() {
-        /*Returns the coordinate of north edge of road*/
+
         return ynorth;
     }
 
     public int getYsouth() {
-        /*Returns the coordinate of south edge of road*/
+
         return ysouth;
     }
 
@@ -399,159 +383,3 @@ public class Road {
     }
 
 }
-
-
-/*Old code i wasnt ready to delete   */
-/*
-    public void addTrafficLight(String which_end) {
-        if (which_end == "Start") {
-            if (direction == "North") {
-                trafficLights.add(new TrafficLight(xwest, ysouth, "NS", "Start"));
-            } else if (direction == "South") {
-                trafficLights.add(new TrafficLight(xeast, ynorth, "NS", "Start"));
-            } else if (direction == "East") {
-                trafficLights.add(new TrafficLight(xwest, ynorth, "EW", "Start"));
-            } else if (direction == "West") {
-                trafficLights.add(new TrafficLight(xeast, ysouth, "EW", "Start"));
-            }
-        } else if (which_end == "End") {
-            if (direction == "North") {
-                trafficLights.add(new TrafficLight(xwest, ynorth, "NS", "End"));
-
-            } else if (direction == "South") {
-                trafficLights.add(new TrafficLight(xeast, ysouth, "NS", "End"));
-
-            } else if (direction == "East") {
-                trafficLights.add(new TrafficLight(xeast, ynorth, "EW", "End"));
-
-            } else if (direction == "West") {
-                trafficLights.add(new TrafficLight(xwest, ysouth, "EW", "End"));
-            }
-        }
-        tot_TrafficLights++;
-
-    }*/
-
-/*    private boolean checkCollision(int vRef) {
-        *//*Returns true if a vehicle will move past the vehicle
-         * in front of it. If it does, position is set to the end of front vehicle
-         * and speed is set to the first vehicles speed. Used in move vehicles function *//*
-        boolean status = false;
-        if (vehicles.size() == 1) {
-            status = false;
-        } else if (vRef == 0) {
-            status = false;
-        } else {
-            if (direction == "North") {
-                if (vehicles.get(vRef).getYpos() + vehicles.get(vRef).getSpeed() >=
-                        vehicles.get(vRef - 1).getYrear()) {
-                    status = true;
-                } else status = false;
-
-            } else if (direction == "East") {
-                if (vehicles.get(vRef).getXpos() + vehicles.get(vRef).getSpeed() >=
-                        vehicles.get(vRef - 1).getXrear()) {
-                    status = true;
-                } else status = false;
-
-            } else if (direction == "West") {
-                if (vehicles.get(vRef).getXpos() - vehicles.get(vRef).getSpeed() <=
-                        vehicles.get(vRef - 1).getXrear()) {
-                    status = true;
-                } else status = false;
-
-            } else if (direction == "South") {
-                if (vehicles.get(vRef).getYpos() - vehicles.get(vRef).getSpeed() <=
-                        vehicles.get(vRef - 1).getYrear()) {
-                    status = true;
-                } else status = false;
-
-            }
-
-        }
-        return status;
-    }*/
-
-
-
-
- /*   public void moveVehicles() {
-        for (int i = 0; i < vehicles.size(); i++) {
-
-            if (vehicles.get(i).getDirection() == "North") {
-                if (checkCollision(i)) {
-                    vehicles.get(i).setSpeed(vehicles.get(i - 1).getSpeed());
-                    vehicles.get(i).setPos(vehicles.get(i - 1).getYrear() - 1);
-                } else if (checkTrafficLights(i)) {
-                    if (trafficLights.get(light_ref).isGreen()) {
-                        if (trafficLights.get(light_ref).isEnd()) {
-                            isOffRoad(i, vehicles.get(i).getRoad_ref() + 1);
-                        }
-                    } else vehicles.get(i).setPos(trafficLights.get(light_ref).getYpos() - 1);
-
-                } else if (vehicles.get(i).getYpos() + vehicles.get(i).getSpeed() > ynorth) {
-                    isOffRoad(i, vehicles.get(i).getRoad_ref() + 1);
-
-                } else vehicles.get(i).moveVehicle();
-
-            } else if (vehicles.get(i).getDirection() == "South") {
-                if (checkCollision(i)) {
-                    vehicles.get(i).setSpeed(vehicles.get(i - 1).getSpeed());
-                    vehicles.get(i).setPos(vehicles.get(i - 1).getYrear() + 1);
-                } else if (checkTrafficLights(i)) {
-                    if (trafficLights.get(light_ref).isGreen()) {
-                        if (trafficLights.get(light_ref).isEnd()) {
-                            isOffRoad(i, vehicles.get(i).getRoad_ref() + 1);
-                        }
-                    } else vehicles.get(i).setPos(trafficLights.get(light_ref).getYpos() - 1);
-
-                } else if (vehicles.get(i).getYpos() - vehicles.get(i).getSpeed() < ysouth) {
-                    isOffRoad(i, vehicles.get(i).getRoad_ref() + 1);
-
-                } else vehicles.get(i).moveVehicle();
-
-            } else if (vehicles.get(i).getDirection() == "East") {
-                if (checkCollision(i)) {
-                    vehicles.get(i).setSpeed(vehicles.get(i - 1).getSpeed());
-                    vehicles.get(i).setPos(vehicles.get(i - 1).getXrear() - 1);
-                } else if (checkTrafficLights(i)) {
-                    if (trafficLights.get(light_ref).isGreen()) {
-                        if (trafficLights.get(light_ref).isEnd()) {
-                            isOffRoad(i, vehicles.get(i).getRoad_ref() + 1);
-                        }
-                    } else vehicles.get(i).setPos(trafficLights.get(light_ref).getXpos() - 1);
-
-                } else if (vehicles.get(i).getXpos() + vehicles.get(i).getSpeed() > xeast) {
-                    isOffRoad(i, vehicles.get(i).getRoad_ref() + 1);
-                } else vehicles.get(i).moveVehicle();
-
-            } else if (vehicles.get(i).getDirection() == "West") {
-                if (checkCollision(i)) {
-                    vehicles.get(i).setSpeed(vehicles.get(i - 1).getSpeed());
-                    vehicles.get(i).setPos(vehicles.get(i - 1).getXrear() + 1);
-                } else if (checkTrafficLights(i)) {
-                    if (trafficLights.get(light_ref).isGreen()) {
-                        if (trafficLights.get(light_ref).isEnd()) {
-                            isOffRoad(i, vehicles.get(i).getRoad_ref() + 1);
-                        } else vehicles.get(i).moveVehicle();
-                    } else vehicles.get(i).setPos(trafficLights.get(light_ref).getXpos() - 1);
-
-                } else if (vehicles.get(i).getXpos() - vehicles.get(i).getSpeed() < xwest) {
-                    isOffRoad(i, vehicles.get(i).getRoad_ref() + 1);
-                } else vehicles.get(i).moveVehicle();
-
-            }
-
-        }
-    }*/
-
- /*
-    public void addVehicle(String type, int speed){
-        if (type == "Car"){
-            addCar(speed);
-        }else if (type == "Bus"){
-            addBus(speed);
-        }else if (type =="Motorbike"){
-            addMotorbike(speed);
-        }
-    }*/
